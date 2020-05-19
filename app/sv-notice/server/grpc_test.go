28e5b4e09a8636bfd4b9e754e62972fc7d45f8c7
liamA <37907909@qq.com>
2020-05-19 17:36:32 +0800

    feat: http 单元测试；

--- app/sv-notice/server/grpc_test.go
+++ app/sv-notice/server/grpc_test.go
@@ -11,34 +11,34 @@ import (
 )
 
 const (
-	tsGRPCPort =":4401"
-	tsGRPCAddress     = "localhost"+tsGRPCPort
-	defaultName = "client:"
+	tsGRPCPort    = ":4401"
+	tsGRPCAddress = "localhost" + tsGRPCPort
+	defaultName   = "client:"
 )
-
 
 func TestSvGRPC_Health(t *testing.T) {
 	go RunServerGRPC(tsGRPCPort)
-	time.Sleep(time.Microsecond *10)
+	time.Sleep(time.Microsecond * 10)
 	testHealth()
 }
 
 func TestSvGRPC_Email(t *testing.T) {
 	go RunServerGRPC(tsGRPCPort)
-	time.Sleep(time.Microsecond *10)
+	time.Sleep(time.Microsecond * 10)
 	testEmail()
 }
 
 func TestSvGRPC_DingTalkMarkdown(t *testing.T) {
 	go RunServerGRPC(tsGRPCPort)
-	time.Sleep(time.Microsecond *10)
+	time.Sleep(time.Microsecond * 10)
 	testDingTalkMarkdown()
 }
 
 func testHealth() {
 	conn, err := grpc.Dial(tsGRPCAddress, grpc.WithInsecure())
 	if err != nil {
-		log.Fatalf("did not connect: %v", err)
+		log.Printf("did not connect: %v", err)
+		return
 	}
 	defer conn.Close()
 	c := pb2.NewNoticeServiceClient(conn)
@@ -54,32 +54,29 @@ func testHealth() {
 		log.Printf("[ERROR]could not greet: %v", err)
 	}
 	log.Printf("testHealth: %+v \n ", r) //r.Message)
-
 }
 
 func testEmail() {
 	conn, err := grpc.Dial(tsGRPCAddress, grpc.WithInsecure())
 	if err != nil {
-		log.Fatalf("did not connect: %v", err)
+		log.Printf("did not connect: %v", err)
+		return
 	}
 	defer conn.Close()
 	c := pb2.NewNoticeServiceClient(conn)
 	// Contact the server and print out its response.
-	name := defaultName
-	if len(os.Args) > 1 {
-		name += os.Args[1]
-	}
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 	r, err := c.Email(ctx, &pb2.EmailReq{
-		To:                   "56487685@qq.com",
-		FromName:             "xxxFrom",
-		Subject:              "你好！",
-		Type:                 "html",
-		Body:                 `<h5 style="color: red; font-size: 16px;">测试-标题</h5>`,
+		To:       "56487685@qq.com",
+		FromName: "xxxFrom",
+		Subject:  "你好！",
+		Type:     "html",
+		Body:     `<h5 style="color: red; font-size: 16px;">测试-标题</h5>`,
 	})
 	if err != nil {
 		log.Printf("[ERROR]could not greet: %v", err)
+		return
 	}
 	log.Printf("testEmail: %+v \n ", r) //r.Message)
 }
@@ -87,7 +84,8 @@ func testEmail() {
 func testDingTalkMarkdown() {
 	conn, err := grpc.Dial(tsGRPCAddress, grpc.WithInsecure())
 	if err != nil {
-		log.Fatalf("did not connect: %v", err)
+		log.Printf("did not connect: %v", err)
+		return
 	}
 	defer conn.Close()
 	c := pb2.NewNoticeServiceClient(conn)
@@ -99,14 +97,16 @@ func testDingTalkMarkdown() {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 	r, err := c.DingTalkMarkdown(ctx, &pb2.DingTalkReq{
-		Token:                "xxx1",
-		RobotSecret:          "xxx2",
-		Title:                "title-标题",
-		Text:                 "text-",
-		IsAtAll:              false,
+		Token:       "xxx1",
+		RobotSecret: "xxx2",
+		Title:       "title-标题",
+		Text:        "text-",
+		IsAtAll:     false,
 	})
 	if err != nil {
 		log.Printf("[ERROR]could not greet: %v", err)
+		return
 	}
 	log.Printf("dingTalkMarkdown: %+v \n ", r) //r.Message)
 }
+
--- app/sv-notice/server/http_test.go
+++ app/sv-notice/server/http_test.go
@@ -1,3 +1,34 @@
 package server
 
+import (
+	"github.com/go-liam/util/request/tshttp"
+	"strings"
+	"testing"
+	"time"
+)
 
+const (
+	tsHTTPPort    = ":4402"
+	tsHTTPAddress = "http://localhost" + tsHTTPPort
+)
+
+func TestHTTPIndex(t *testing.T) {
+	go RunServerGin(tsHTTPPort)
+	time.Sleep(time.Microsecond * 10)
+	tsIndex()
+}
+
+func tsIndex()  {
+	sv := new(tshttp.HTTPTest)
+	url := tsHTTPAddress+"/"
+	data := `
+		{
+			"ID":1,
+			"versionId":101
+		}
+		`
+	reader := strings.NewReader(data)
+	code, back, _ := sv.HTTPData(tshttp.MethodHTTPGET,url,reader,"","")
+	println("code:",code)
+	println(back)
+}
